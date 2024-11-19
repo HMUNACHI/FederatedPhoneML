ea342eef19714278993de44c8c6c246d3c035eab
rshemet <shemet0roman@gmail.com>
2024-11-19 00:20:27 +0000

    root App, Homepage and training toggle placeholder

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { styledTheme } from './styles/theme';
+import { ThemeProvider } from 'styled-components/native';
+import Home from './components/Home';
+import theme from './styles/theme';
+
 import {isAvailable, train, evaluate, predict} from './ferra';
+
+const App = () => {
+    return (
+        <SafeAreaView  style={{ flex: 1, backgroundColor: theme.colors.background }}>
+        <ThemeProvider theme={styledTheme}>
+            <Home/>
+        </ThemeProvider>
+        </SafeAreaView>
+    );
+};
+
+export default App;
