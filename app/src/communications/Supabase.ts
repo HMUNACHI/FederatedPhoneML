311dbd07cb1939de8b50aedd356baebd95c13e86
JetsettingJames <unsworth.jamesa@gmail.com>
2024-11-22 20:05:50 +0000

    Adding .env for supabase creds

--- app/src/communications/Supabase.ts
+++ app/src/communications/Supabase.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env';
 
-const supabaseUrl = '';
-const supabaseAnonKey = '';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
