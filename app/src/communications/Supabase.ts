200894700e1d865c965404961109ab98130e6273
rshemet <shemet0roman@gmail.com>
2024-11-24 22:23:59 +0000

    registering device with supabase on successful login

--- app/src/communications/Supabase.ts
+++ app/src/communications/Supabase.ts
@@ -2,3 +2,18 @@ import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export async function insertRow(table: string, data: Record<string, any>): Promise<void> {
+    'Inserts any data object into a specified supabase table'
+    try {
+      const { error } = await supabase.from(table).insert([data]);
+  
+      if (error) {
+        throw error;
+      }
+  
+      console.log(`Row inserted successfully into ${table}`);
+    } catch (err) {
+      console.error('Error inserting row:', err);
+    }
+}
