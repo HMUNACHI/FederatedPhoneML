cd8ff6485c6d9956749d89364846acf90295173b
rshemet <shemet0roman@gmail.com>
2024-11-25 20:21:01 +0000

    moving supabase function to Supabase.ts

--- app/src/communications/Supabase.ts
+++ app/src/communications/Supabase.ts
@@ -63,3 +63,40 @@ export async function toggleDeviceAvailability( currentAvailability:string ): Pr
       return newAvailability
   }
 }
+
+export const registerDeviceWithSupabase = async () => {
+  // https://www.notion.so/TODO-Engineering-reminders-148d8e920c9780fb8af0c558296e1bd2?pvs=4#148d8e920c9780cea520d862b57c2f1c
+  try {
+    // Step 1: Get the user session
+    const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
+    if (sessionError) {
+      throw sessionError;
+    }
+    const user_id: string | undefined = sessionData.session?.user.id;
+    if (!user_id) {
+      throw new Error('User session is invalid or user ID is missing.');
+    }
+    // Step 2: Check whether this user already has a registered device
+    const { data: deviceData, error: tableLoadError } = await supabase
+      .from('devices')
+      .select('id') 
+      .eq('user_id', user_id)
+      .limit(1); 
+
+    if (tableLoadError) {
+      throw tableLoadError;
+    }
+
+    // Step 3. only register this device if there's no device record for this user yet
+    if (deviceData && deviceData.length === 0) {
+      await insertRow('devices', {
+          user_id: user_id,
+          status: 'available',
+          last_updated: new Date(),
+        },
+      );
+    }
+  } catch (err) {
+    console.error('Error registering device with Supabase:', err);
+  }
+}
