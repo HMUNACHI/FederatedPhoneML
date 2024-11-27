06f472a66d98cddfb12f315c90d1c47bccb2c980
rshemet <shemet0roman@gmail.com>
2024-11-27 11:37:04 +0000

    deployment settings

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,15 +1,5 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const {getDefaultConfig} = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-// module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = (() => {
-  const defaultConfig = getDefaultConfig(__dirname);
-  const {assetExts} = defaultConfig.resolver;
-  return {
-    resolver: {
-      // Add bin to assetExts
-      assetExts: [...assetExts, 'bin'],
-    }
-  };
-})();
+module.exports = config;
