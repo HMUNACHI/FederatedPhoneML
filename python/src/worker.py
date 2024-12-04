6a41d54c9fc81964d847fadee595ee23233fe8f0
Henry Ndubuaku <ndubuakuhenry@gmail.com>
2024-12-04 16:06:20 +0000

    safety

--- python/src/worker.py
+++ python/src/worker.py
@@ -5,8 +5,6 @@ from typing import List, Optional, Dict
 from supabase import create_client, Client
 from realtime._async.client import AsyncRealtimeClient
 
-SUPABASE_URL = "https://qunrxbelodkkjibgrvnz.supabase.co"
-SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1bnJ4YmVsb2Rra2ppYmdydm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1NTc1MTMsImV4cCI6MjA0NjEzMzUxM30.Mi-q2P2XwFS-2i_gMtx4khnN0cgVkA4pf7PbA5o0xIU"
 
 supabase : Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
