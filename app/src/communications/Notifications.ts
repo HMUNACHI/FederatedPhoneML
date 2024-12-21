7ad30137da418da3d3cd31cf34c923b8c71acda5
rshemet <shemet0roman@gmail.com>
2024-12-21 13:04:23 +0400

    explicitly specifying project ID

--- app/src/communications/Notifications.ts
+++ app/src/communications/Notifications.ts
@@ -1,6 +1,7 @@
 import * as Notifications from 'expo-notifications';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Sentry from '@sentry/react-native';
+import Constants from 'expo-constants';
 
 export async function registerForPushNotificationsAsync() : Promise<void | null> {
   const session = await AsyncStorage.getItem('user_session');
@@ -21,7 +22,10 @@ export async function registerForPushNotificationsAsync() : Promise<void | null>
       return null;
     }
 
-    const pushToken = (await Notifications.getExpoPushTokenAsync()).data;
+    const projectId = Constants.expoConfig.extra.eas.projectId;
+    const pushToken = (await Notifications.getExpoPushTokenAsync(
+      projectId
+    )).data;
     if (pushToken) {
       try {
         const response = await fetch('https://qunrxbelodkkjibgrvnz.supabase.co/rest/v1/tokens?on_conflict=user_id', {
